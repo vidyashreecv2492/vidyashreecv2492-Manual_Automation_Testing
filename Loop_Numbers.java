1b405701a5010096dc78b2df47740912fa940612
vidyashree c v <69243283+vidyashreecv2492@users.noreply.github.com>
2025-06-10 20:51:16 +0530

    Basic code for "For loop"
    Print numbers from 1 to 100, but skip multiples of 5
    Write a program to print all numbers from 1 to 100, but skip the numbers that are multiples of 5.
    Example:
    Output: 1 2 3 4 6 7 8 9 11 ... (skip 5, 10, 15…)
    Tip: Use "continue" inside a loop

---
+++ Loop_Numbers.java
@@ -0,0 +1,19 @@
+package MyPackage;
+
+public class Loop_Numbers {
+
+	public static void main(String[] args) {
+		for (int i = 1 ; i <= 100; i++) {
+			if(i % 5 == 0) {
+				
+				continue;
+				
+				
+			}
+			System.out.print(i + " ");
+			
+			
+		}
+	}
+
+}
