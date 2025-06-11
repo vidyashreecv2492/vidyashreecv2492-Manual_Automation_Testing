bb1c008d247d7ebcee083500b9ba1379dba35ad2
vidyashree c v <69243283+vidyashreecv2492@users.noreply.github.com>
2025-06-11 20:56:59 +0530

    basic code for multiplication
    Print multiplication table for 9

---
+++ Multiply.java
@@ -0,0 +1,14 @@
+package MyPackage;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		int num = 9;
+		for(int i=1; i<=10; i++) {
+			System.out.println(num + " * "+ i + " = " + (num * i ));
+		}
+		
+
+	}
+
+}
