db7fd2879620ba38f0b7d3ccb5092fbe342e3248
vidyashree c v <69243283+vidyashreecv2492@users.noreply.github.com>
2025-06-18 19:19:39 +0530

    Count letters in string
    Count how many times a character appears
    Example: Input: "banana", char = 'a' Output: 3

---
+++ CountCharacters.java
@@ -0,0 +1,18 @@
+package MyPackage;
+
+public class CountCharacters {
+
+	public static void main(String[] args) {
+		String str = "banana";
+		char ch = 'a';
+		int count = 0;
+		
+		for(int i=0; i < str.length(); i++) {
+			if(str.charAt(i) == ch) {
+				count++;
+			}
+		}
+		System.out.println("Character " + ch + " appears " +count +" times");
+	}
+
+}
